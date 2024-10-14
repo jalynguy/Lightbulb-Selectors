5a2c4dd9de2565fd823bb689aca20a6e7f69a5be
James Nguyen <james.nguyen@james.nguyen-MacBook-Pro>
2024-10-14 11:00:54 -0700

    Submitting HTMl project

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Write your code here
+let count = 0;
+let subtitle = '';
+function toggleOn(id){
+    let lightBulb = document.getElementById(id);
+    lightBulb.classList.toggle("active");
+    count += 1;
+    if(count == 1){
+        subtitle = `You've clicked the lights 1 time`;
+    }
+    else{
+        subtitle = `You've clicked the lights ${count} times`;
+    }
+    document.querySelector('h3').innerHTML = subtitle;
+    return;
+}
+
